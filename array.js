6f25de8516c77d2f1e09afe44e46dfd2773e54ed
prince-goti <princegoti40@gmail.com>
2023-07-16 23:51:26 +0530

    array practice

---
+++ array.js
@@ -0,0 +1,55 @@
+//array declaration
+/*let a=[1,2,3,'prince',false];
+console.log(a);
+
+let b=new Array(23,24,35,'prince',true);
+console.log(b);
+
+//position
+console.log(a[4]);
+
+//change the element of array
+a[4]='jaymin';
+console.log(a);
+  
+//********************************************** 
+//array methods
+let arr=[1,2,3,4,5,6];
+console.log(arr);
+
+arr.pop();      //remove a last position of array
+console.log(arr);
+
+arr.push(6,7);   //add a last position of array
+console.log(arr);
+
+arr.shift();     //remove a first position of array
+console.log(arr);
+
+let f=arr.shift();  //what is a remove
+console.log(f);
+
+arr.unshift(1,2);   //add a first position of array
+console.log(arr);
+
+ let a=[11,22,33,44];
+ let b=[55,66,77];
+
+ let c=a.concat(b);  //merge a two array
+ console.log(c);
+
+ let s= c.join("!");  // convert to string
+ console.log(s);
+
+ c.reverse();   //reverse to array
+ console.log(c);
+
+ console.log(c.indexOf(44));  //find a particular position of element
+
+ arr1=[1,2,3,4,5,6,7,8];
+
+ //console.log(arr1.slice(2,6));   //diplay a paticular line of array
+
+ arr1.splice(2,1,9);           // add and delete a particular position
+
+ console.log(arr1);*/
