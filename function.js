9afd5b5046c75eae25ddc189ee257f7da214a88a
prince-goti <princegoti40@gmail.com>
2023-07-19 23:51:21 +0530

    function

---
+++ function.js
@@ -0,0 +1,104 @@
+//  simple diclaration of function
+/*function intro(){
+    console.log("hi there");
+    console.log("my name is prince goti");
+}
+//   use parameter and return diclaration of function 
+function sqr(x){
+    //console.log(x*x);
+    return x*x;
+}
+intro();    //simple function
+
+sqr(6);     //argument calling function
+
+let a=sqr(8)
+console.log(a);       //store the variable and cALL FUNCTION
+
+//***************************************************************** 
+
+// function without parameter
+
+function pasmsg(){
+    return "my name is a prince goti."
+
+}
+
+let p = pasmsg();
+
+console.log(p);
+
+let nm = p + "i learn a javascript";
+
+console.log(nm);
+
+//***********************************************************************
+
+// function with a paramiter
+
+ function ToShow(x="i learn a javascript")    //single parameter
+ {
+     console.log(x);
+}
+
+ ToShow("my name is p");
+
+ ToShow(); 
+
+function sum(x,y = 20)    //defalut paramiter
+{
+    return x + y;
+}
+ 
+let s = sum (10);
+
+console.log(s);
+
+//************************************************************************** 
+
+// arrow function
+
+const square = (x) => x * x;      //one parameter
+
+const sumoftwonum = (x,y) => x + y;   //multiple paarmeter
+
+const sum =  (x,y)  =>                // multiple statement
+{
+    console.log("sum is ");
+    return x+y;
+}
+
+const sumanddiff =(x,y) => ({sum:x+y , diff:x-y});      //return in object
+
+let output1 = square(5);
+console.log(output1);
+
+let output2 = sumoftwonum(20,10);
+console.log(output2);
+
+let output3 = sum(10,10);
+console.log(output3);
+
+let output4 = sumanddiff(5,5);
+console.log(output4);    
+
+//**************************************************************************
+
+//anynomous function
+
+let p = function (){
+    console.log("my name is prince goti");  
+}
+p();
+
+// at that and one time run
+(function exe(x){
+    console.log(x*x);
+})(5);                    */
+
+
+
+
+
+
+
