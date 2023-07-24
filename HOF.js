fc8fc7101265f9535cc94958aefbda341b72b6be
prince-goti <princegoti40@gmail.com>
2023-07-24 23:00:09 +0530

    high order function

---
+++ HOF.js
@@ -0,0 +1,31 @@
+// const powertwo = (n) => {
+//     return n ** 2
+// }
+// // console.log(powertwo(5))
+
+// function powercube(powertwo , n){
+//          return powertwo(n)* n
+// }
+
+// console.log(powercube(powertwo,5))
+
+// const onefun = m => {
+//           twofun = n =>{
+//             threefun = p =>{
+//                  return m + n + p
+//             }
+//             return threefun
+//        }
+//        return twofun
+// }
+
+
+// console.log(onefun(2)(4)(6))
+
+function hello(){
+    console.log("prince goti !", Math.random());
+}
+
+setInterval(hello, 1000)
+
+//setTimeout(hello, 2000)
